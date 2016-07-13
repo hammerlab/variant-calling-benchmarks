e381e62d2a9dccf2ce60a5416023de1b752e4985
Tim O'Donnell <timodonnell@gmail.com>
2016-07-12 23:46:55 -0400

    better comments

--- variant_calling_benchmarks/config.py
+++ variant_calling_benchmarks/config.py
@@ -2,9 +2,15 @@ import os
 import logging
 import string
 import json
-import functools
 
 def load_config(*filenames):
+    '''
+    Load the specified JSON config files.
+
+    Returns
+    ---------
+    MergedConfigDicts
+    '''
     dicts = []
     substitutions = {}
     for filename in filenames:
@@ -24,6 +30,9 @@ def load_config(*filenames):
     return MergedConfigDicts(dicts)
 
 class MergedConfigDicts(dict):
+    '''
+
+    '''
     def __init__(self, dicts):
         self.key_to_dict = {}
         for d in dicts:
@@ -35,13 +44,46 @@ class MergedConfigDicts(dict):
         return self.key_to_dict[key].get_substituted(key, path=path)
 
 class ConfigDict(dict):
+    '''
+    Thin wrapper over a dict that adds the get_substituted method.
+    '''
     def __init__(self, filename, substitutions, dictionary):
+        '''
+        Parameters
+        -------------
+        filename : string
+            File this dict was loaded from.
+
+        substitutions : dict
+            String substitutions that should be used when get_substituted
+            is called.
+
+        dictionary : dict
+            The dictionary to be wrapped.
+        '''
         self.filename = filename
         self.substitutions = substitutions
         self.basedir = os.path.dirname(self.filename)
         self.update(dictionary)
 
     def get_substituted(self, key, path=False):
+        '''
+        Return the specified key from the dict after running string
+        substitutions.
+
+        Parameters
+        -------------
+        path : boolean
+            If True, then after template expansion on the value, it will
+            additionally be treated as a path. If it is relative (does not
+            start with /) then the directory of the JSON file the Config was
+            loaded from will be preprended to it.
+
+        Returns
+        -------------
+        string, number, list, or ConfigDict giving the requested value
+
+        '''
         value = self[key]
         result = string.Template(value).substitute(**self.substitutions)
         if path:
